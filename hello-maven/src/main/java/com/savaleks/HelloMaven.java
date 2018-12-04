3f1956d4a79367c32edd246474df702108c182b6
Alexander <savaleks@gmail.com>
2018-12-04 14:05:42 +0200

    Spring Maven project

---
+++ hello-maven/src/main/java/com/savaleks/HelloMaven.java
@@ -0,0 +1,14 @@
+package com.savaleks;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HelloMaven {
+
+    private final static Logger log = LoggerFactory.getLogger(HelloMaven.class);
+
+    public static void main(String[] args) {
+        log.info("Hello INFO");
+        log.info("Hello DEBUG");
+    }
+}
